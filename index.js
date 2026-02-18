cd9d22101739217b44a6efc87793636442ef331b
Abhishek kumar <abhishekkumar@Abhisheks-MacBook-Air-3.local>
2026-02-18 23:25:36 +0530

    start

---
+++ index.js
@@ -0,0 +1,26 @@
+const { SMTPServer } = require("smtp-server");
+
+const server = new SMTPServer({
+    allowInsecureAuth:true,
+    authOptional:true,
+    onConnect(session, cb) {
+        console.log(`on connect part run -- session ${session?.id}`);
+        cb()
+    },
+    onMailFrom(address, session, cb) {
+        console.log(`on main from run. - ${address?.address}, ${session?.id}`);
+        cb()
+    },
+    onRcptTo(address, session, cb) {
+        console.log(`on rcpt to  run. - ${address?.address}, ${session?.id}`);
+        cb()
+    },
+    onData(stream, session, cb) {
+        stream.on('data', data => console.log(`on data run - ${data.toString()}`))
+        stream.on('end', cb())
+
+    }
+});
+
+
+server.listen(25, () => { console.log('--| smpt server running on th epor t o 25 |--') })
