92a5cd4aaf1cad2b6cdf570cfef865a9d2da290c
abkr020 <abkr.020@gmail.com>
2026-02-20 00:35:10 +0530

    file format

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ const { connectDB } = require("./config/db");
 const { createSMTPServer } = require("./smtp/smtpServer");
 
 async function start() {
-  await connectDB(process.env.MONGO_URI, process.env.DB_NAME);
+    await connectDB(process.env.MONGO_URI, process.env.DB_NAME);
 
-  const server = createSMTPServer();
+    const server = createSMTPServer();
 
-  server.listen(process.env.SMTP_PORT, () => {
-    console.log(
-      `🚀 SMTP server running on port ${process.env.SMTP_PORT}`
-    );
-  });
+    server.listen(process.env.SMTP_PORT, () => {
+        console.log(
+            `🚀 SMTP server running on port ${process.env.SMTP_PORT}`
+        );
+    });
 }
 
 start().catch(err => {
-  console.error("❌ Failed to start server:", err);
-  process.exit(1);
+    console.error("❌ Failed to start server:", err);
+    process.exit(1);
 });
